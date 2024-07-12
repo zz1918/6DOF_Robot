3b43ace7e4fbdb8fe179b81bacd2e32cc109c5d1
zz1918 <zz1918@nyu.edu>
2024-07-13 00:48:00 +0800

    bimap

--- Bimap/bimap.cpp
+++ Bimap/bimap.cpp
@@ -4,9 +4,9 @@
 #include <map>
 using namespace std;
 
-template<typename key1, typename key2, typename cont>
+template<typename key1, typename key2, typename type>
 class bimap {
-    map<key1, map<key2, cont>> bim;
+    map<key1, map<key2, type>> bim;
 public:
     bimap() {}
     bool find(key1 k, key2 l)
@@ -19,14 +19,14 @@ public:
             return false;
         return true;
     }
-    bool insert(key1 k, key2 l, cont c)
+    bool insert(key1 k, key2 l, type c)
     {
         if (find(k, l))
             return false;
         bim[k][l] = c;
         return true;
     }
-    cont coeff(key1 k, key2 l)
+    type coeff(key1 k, key2 l)
     {
         return bim[k][l];
     }
